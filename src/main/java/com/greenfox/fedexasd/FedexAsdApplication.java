72273c53e451255ed83a38ba485ee2fc8e07e68e
Diána Szántó <szanto.diana.1992@gmail.com>
2021-04-12 12:04:10 +0200

    Initial commit

---
+++ src/main/java/com/greenfox/fedexasd/FedexAsdApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.fedexasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedexAsdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FedexAsdApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/greenfox/fedexasd/FedexAsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.fedexasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedexAsdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
